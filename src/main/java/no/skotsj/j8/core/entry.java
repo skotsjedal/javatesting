392f1ae4c35326abb54e52bbb123f8d5afb9a394
Andreas <aokaado@gmail.com>
2014-03-08 01:34:45 +0100

    Formatting fix

--- src/main/java/no/skotsj/j8/core/entry.java
+++ src/main/java/no/skotsj/j8/core/entry.java
@@ -17,11 +17,11 @@ public class entry
   {
     ArrayList<String> testlist = Lists.newArrayList("one", "two", "three", "six or nine", "flea", "Burpees");
 
-    Collections.sort(testlist, (x, y) -> x.charAt(0) - y.charAt(0)); testlist.stream().map(l -> l.substring(2))
-          .mapToInt(String::length).average().ifPresent(out::println);
+    Collections.sort(testlist, (x, y) -> x.charAt(0) - y.charAt(0));
+    testlist.stream().map(l -> l.substring(2)).mapToInt(String::length).average().ifPresent(out::println);
 
-    testlist.forEach(out::println); "Welcome to the league of draven".chars().distinct().map(
-          c -> testlist.stream().anyMatch(s -> s.contains(Character.toString((char) c))) ? c : c + 1).forEach(
-          c -> out.printf("%c-", c));
+    testlist.forEach(out::println);
+    "Welcome to the league of draven".chars().distinct().map(c -> testlist.stream().anyMatch(s -> s.contains(
+            Character.toString((char) c))) ? c : c + 1).forEach(c -> out.printf("%c-", c));
   }
 }
